5f236c1af807c30f52109d6b7146f4c277bdca6c
sivasankar10 <111382075+sivasankar10@users.noreply.github.com>
2023-09-14 09:43:14 +0530

    Create caesar cipher.c

---
+++ caesar cipher.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+char plain[100], cipher[100];
+int key,i,length;
+int result;
+printf("\n Enter the plain text:");
+scanf("%s", plain);
+printf("\n Enter the key value:");
+scanf("%d", &key);
+printf("\n \n \t Encrypted text: ");
+for(i = 0, length = strlen(plain); i < length; i++)
+{
+cipher[i]=plain[i] + key;
+if (isupper(plain[i]) && (cipher[i] > 'Z'))
+cipher[i] = cipher[i] - 26;
+if (islower(plain[i]) && (cipher[i] > 'z'))
+cipher[i] = cipher[i] - 26;
+printf("%c", cipher[i]);
+}
+printf("\n \n \t Decrypted text: ");
+for(i=0;i<length;i++)
+{
+plain[i]=cipher[i]-key;
+if(isupper(cipher[i])&&(plain[i]<'A'))
+plain[i]=plain[i]+26;
+if(islower(cipher[i])&&(plain[i]<'a'))
+plain[i]=plain[i]+26;
+printf("%c",plain[i]);
+}
+
+}
